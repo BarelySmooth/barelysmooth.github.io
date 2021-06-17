1ae21c0d0e89fb07d407a9b39376fee80c1f91c6
BarelySmooth <83728060+BarelySmooth@users.noreply.github.com>
2021-06-17 14:20:39 +0400

    Add footer

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function closeIntro() {
     const introElement = document.getElementById("intro");
+    const introElementBehind = document.getElementById("intro-back");
     introElement.style["display"] = "none";
+    introElementBehind.style["display"] = "none";
 }
